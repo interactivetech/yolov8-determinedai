580b3fc6a71160005f59099965cefc4cefc81583
andrew <inter>
2023-01-17 18:55:13 +0000

    update

---
+++ run_exp_coco_full.sh
@@ -0,0 +1,1 @@
+det -m http://demo.determined.ai:8080/ experiment create -f run_full_coco.yaml . 
--- startup-hook.sh
+++ startup-hook.sh
@@ -2,3 +2,4 @@ apt-get update
 apt-get install ffmpeg libsm6 libxext6  -y
 pip install ultralytics
 pip install terminaltables
+bash get_coco.sh
